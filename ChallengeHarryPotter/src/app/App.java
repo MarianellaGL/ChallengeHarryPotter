8f19c9921a2ee6c5d52aac37f55fb4352ee53af1
MarianellaGL <marianella.gomezluna@gmail.com>
2019-08-27 12:13:17 -0300

    cambio en app

--- ChallengeHarryPotter/src/app/App.java
+++ ChallengeHarryPotter/src/app/App.java
@@ -7,22 +7,15 @@ import app.personajes.Personaje;
 public class App {
     public static void main(String[] args) throws Exception {
 
-
         System.out.println("Inicializando juego");
         JuegoHarryPotter.InicializarJuego();
 
-        Mago pj1 =(Mago) JuegoHarryPotter.ElegirPersonaje("Harry Potter");
+        Mago pj1 = (Mago) JuegoHarryPotter.ElegirPersonaje("Harry Potter");
 
-        Mago pj2 =(Mago) JuegoHarryPotter.ElegirPersonaje("Severus Snape");
+        Mago pj2 = (Mago) JuegoHarryPotter.ElegirPersonaje("Severus Snape");
 
+        pj2.atacar(pj1, );
 
-        pj2.atacar(pj1, hechiAtaque);
-
-    
-        }
-
-    
     }
 
-   
 }
