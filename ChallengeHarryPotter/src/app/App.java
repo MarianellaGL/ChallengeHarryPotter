8b52182c9e8f3ce43bd6d1348824d905ce61cda2
MarianellaGL <marianella.gomezluna@gmail.com>
2019-08-30 15:23:18 -0300

    prints

--- ChallengeHarryPotter/src/app/App.java
+++ ChallengeHarryPotter/src/app/App.java
@@ -16,80 +16,97 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         System.out.println("Inicializando juego");
-        
+
         JuegoHarryPotter.InicializarJuego();
 
         Teclado.nextLine();
 
-
+        System.out.println("Seleccione 1 o 2 para elegir al personaje");
         Mago pj1 = (Mago) JuegoHarryPotter.ElegirPersonaje("Harry Potter");
-
-        System.out.println("Seleccione letra para elegir al personaje");
-        
-
         Mago pj2 = (Mago) JuegoHarryPotter.ElegirPersonaje("Severus Snape");
+        int opcion = Teclado.nextInt();
+        Teclado.nextLine();
 
-        SectumSempra hechiAtaque = new SectumSempra();
-        hechiAtaque.nombreHechizo = "Sectum Sempra";
-        hechiAtaque.esOscuro = true;
-        hechiAtaque.nivelDaño = 100;
+        switch (opcion) {
+        case 1:
+            System.out.println("Elegiste a Harry Potter");
 
-        JuegoHarryPotter.hechizos.add(hechiAtaque);
+            break;
 
-        CaveInimicum hechiDefensa = new CaveInimicum();
-        hechiDefensa.nombreHechizo = " Cave Inimicum";
-        hechiDefensa.esOscuro = false;
-        hechiDefensa.nivelDaño = 0;
-        hechiDefensa.nivelDefensa = 75;
+        case 2:
+            System.out.println("Elegiste a Severus Snape");
 
-        JuegoHarryPotter.hechizos.add(hechiDefensa);
+            break;
 
-        AvadaKedabra kedabra = new AvadaKedabra();
-        kedabra.nombreHechizo = "Avada Kedabra";
-        kedabra.esOscuro = true;
-        kedabra.nivelDaño = 100000;
+        case 3:
 
-        JuegoHarryPotter.hechizos.add(kedabra);
+            break;
 
-        MaleficioCruciatus cruciatus = new MaleficioCruciatus();
-        cruciatus.nombreHechizo = "Maleficio Cruciatus";
-        cruciatus.esOscuro = true;
-        cruciatus.nivelDaño = 300;
-
-        // tenemos que hacer que pj1 y pj 2 peleen y que alguno de los dos gane o
-        // pierda, por ahora probemos algo tranqui.
-        System.out.println("La salud de " + pj1.nombre + " es de " + pj1.salud);
-        pj2.atacar(pj1, hechiAtaque);
-        System.out.println("La salud de " + pj2.nombre + " es de " + pj2.salud);
-    
-        boolean atacapj1 = true;
-
-    while (pj1.salud > 0 && pj2.salud >0){
+        default:
+            System.out.println("No hay más personajes");
+            break;
 
 
-        if(atacapj1){
+        }{
 
-            if(pj1 instanceof IHacerMagia){
+            SectumSempra hechiAtaque = new SectumSempra();
+            hechiAtaque.nombreHechizo = "Sectum Sempra";
+            hechiAtaque.esOscuro = true;
+            hechiAtaque.nivelDaño = 100;
 
-                IHacerMagia mago1 = (IHacerMagia)pj1;
-                mago1.atacar(pj2, hechiAtaque);
+            JuegoHarryPotter.hechizos.add(hechiAtaque);
+
+            CaveInimicum hechiDefensa = new CaveInimicum();
+            hechiDefensa.nombreHechizo = " Cave Inimicum";
+            hechiDefensa.esOscuro = false;
+            hechiDefensa.nivelDaño = 0;
+            hechiDefensa.nivelDefensa = 75;
+
+            JuegoHarryPotter.hechizos.add(hechiDefensa);
+
+            AvadaKedabra kedabra = new AvadaKedabra();
+            kedabra.nombreHechizo = "Avada Kedabra";
+            kedabra.esOscuro = true;
+            kedabra.nivelDaño = 100000;
+
+            JuegoHarryPotter.hechizos.add(kedabra);
+
+            MaleficioCruciatus cruciatus = new MaleficioCruciatus();
+            cruciatus.nombreHechizo = "Maleficio Cruciatus";
+            cruciatus.esOscuro = true;
+            cruciatus.nivelDaño = 300;
+
+            // tenemos que hacer que pj1 y pj 2 peleen y que alguno de los dos gane o
+            // pierda, por ahora probemos algo tranqui.
+            System.out.println("La salud de " + pj1.nombre + " es de " + pj1.salud);
+            pj2.atacar(pj1, hechiAtaque);
+            System.out.println("La salud de " + pj2.nombre + " es de " + pj2.salud);
+
+            boolean atacapj1 = true;
+
+            while (pj1.salud > 0 && pj2.salud > 0) {
+
+                if (atacapj1) {
+
+                    if (pj1 instanceof IHacerMagia) {
+
+                        IHacerMagia mago1 = (IHacerMagia) pj1;
+                        mago1.atacar(pj2, hechiAtaque);
+                    }
+
+                    atacapj1 = false;
+                } else if (pj2 instanceof IHacerMagia) {
+
+                    IHacerMagia mago2 = (IHacerMagia) pj2;
+                    mago2.atacar(pj2, hechiAtaque);
+                }
+
             }
-
-        atacapj1 = false;
-        }else if (pj2 instanceof IHacerMagia){
-
-            IHacerMagia mago2 = (IHacerMagia)pj2;
-            mago2.atacar(pj2, hechiAtaque);
-        }
-
+            System.out.println("La salud de " + pj1.nombre + " es de " + pj1.salud);
+            pj2.atacar(pj1, hechiAtaque);
+            System.out.println("La salud de " + pj2.nombre + " es de " + pj2.salud);
 
         }
-        System.out.println("La salud de " + pj1.nombre + " es de " + pj1.salud);
-        pj2.atacar(pj1, hechiAtaque);
-        System.out.println("La salud de " + pj2.nombre + " es de " + pj2.salud);
-    
-    }      
 
-
-
+    }
 }
